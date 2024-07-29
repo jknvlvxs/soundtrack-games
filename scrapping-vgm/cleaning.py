0bf2c1205927a4b4ba7c66778a1afe3cd656cc74
jknvlvxs <dev.julio.alves@gmail.com>
2024-07-29 17:02:48 -0300

    feat: add cleaning file

---
+++ scrapping-vgm/cleaning.py
@@ -0,0 +1,22 @@
+import os
+
+# Define the base directory
+path = os.path.dirname(__file__)
+base_dir = f"{path}/data/"
+
+# Traverse the directory structure
+for root, dirs, files in os.walk(base_dir):
+    for file in files:
+        if file.endswith(".html"):
+            file_path = os.path.join(root, file)
+
+            # Read the file content
+            with open(file_path, "r", encoding="utf-8") as f:
+                content = f.read()
+
+            # Check if '.7z' is in the content
+            if ".7z" not in content:
+                os.remove(file_path)
+                print(f"Deleted: {file_path}")
+
+print("Completed the cleanup process.")
