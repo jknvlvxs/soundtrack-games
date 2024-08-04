26ba7c5f7518ce3d7c1259ae0099a2c3ce55ee4f
jknvlvxs <dev.julio.alves@gmail.com>
2024-08-04 20:05:16 -0300

    feat: rename files

--- scrapping-vgm/extensions/convert.py
+++ scrapping-vgm/extensions/convert.py
@@ -6,7 +6,7 @@ path = os.path.dirname(__file__)
 with open(f"{path}/extensions.json", "r") as json_file:
     extensions = json.load(json_file)
 
-with open(f"{path}/ffmpeg/encoders.ffmpeg.txt", "r") as encoders_file:
+with open(f"{path}/ffmpeg/formats.ffmpeg.txt", "r") as encoders_file:
     encoders_content = encoders_file.read()
 
 filtered_extensions = [ext for ext in extensions if ext in encoders_content]
