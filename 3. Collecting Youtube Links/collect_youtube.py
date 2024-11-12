bb59aab041e3a8b01d0aca057bfb38e4ffca5c10
jknvlvxs <dev.julio.alves@gmail.com>
2024-11-12 14:50:23 -0300

    feat: implements new collect youtube rule

--- 3. Collecting Youtube Links/collect_youtube.py
+++ 3. Collecting Youtube Links/collect_youtube.py
@@ -4,26 +4,32 @@ from youtubesearchpython import VideosSearch
 import re
 
 path = os.path.dirname(__file__)
-base_dir = os.path.join(path, "")
-data_file = os.path.join(path + "/../2. Generating data/", "data.json")
-
+data_file = os.path.join(path, "metadata.json")
 chunk_size = 100
-
+target_channel = "World of Longplays"
 
 def search_youtube(name, system, query=""):
-    videosSearch = VideosSearch(f"{name} {system} {query}", limit=3)
+    videosSearch = VideosSearch(f"{name} {system} {query}", limit=5)
     results = videosSearch.result()
     if "result" in results and len(results["result"]) > 0:
-        first_video = results["result"][0]
-        video_url = first_video["link"]
-        video_channel = first_video["channel"]["name"]
-        video_title = first_video["title"]
-        duration = first_video["duration"]
+        selected_video = None
+
+        for video in results["result"]:
+            if video["channel"]["name"] == target_channel:
+                selected_video = video
+                break
+
+        if selected_video is None:
+            selected_video = results["result"][0]
+
+        video_url = selected_video["link"]
+        video_channel = selected_video["channel"]["name"]
+        video_title = selected_video["title"]
+        duration = selected_video["duration"]
 
         return video_url, video_channel, video_title, duration
 
     return None, None, None, None
-
 
 def extract_name(name):
     # Remove content inside square brackets and parentheses
@@ -38,48 +44,48 @@ def extract_name(name):
     )
     return cleaned_name
 
-
 def main():
     try:
         with open(data_file, "r", encoding="utf-8") as f:
             data_list = json.load(f)
 
-        new_data_list = []
-
         for i in range(0, len(data_list), chunk_size):
             chunk = data_list[i : i + chunk_size]
             for index, obj in enumerate(chunk):
-                print(f"Searching for nº {i + index + 1} of {len(data_list)} ({int((i + index + 1) * 100/len(data_list))}%)")
-                if "youtube" in obj: continue
+                print(
+                    f"Searching for nº {i + index + 1} of {len(data_list)} ({int((i + index + 1) * 100/len(data_list))}%)"
+                )
 
                 name = extract_name(obj["name"].strip())
                 system = obj["system"].strip()
+                query = "Longplay"
 
-                target_channel = "World of Longplays"
+                video_url, video_channel, video_title, duration = search_youtube(
+                    name, system, query
+                )
 
-                video_url, video_channel, video_title, duration = search_youtube(name, system, target_channel)
+                if video_url is not None:
+                    obj["slug"] = obj["name"]
+                    obj["name"] = name.strip()
 
-                if video_url is not None and video_channel == target_channel:
                     obj["youtube"] = {
                         "url": video_url,
                         "channel": video_channel,
                         "title": video_title,
                         "duration": duration,
                     }
-
-                    new_data_list.append(obj)
                 else:
-                    with open(f"{base_dir}/not_found.log", "a") as f:
+                    with open(os.path.join(path, "not_found.log"), "a") as f:
                         f.write(f"No video found for {name} {system}\n")
 
-            with open(f"{base_dir}/links.json", "w", encoding="utf-8") as f:
-                json.dump(new_data_list, f, indent=4)
+        # Save updated data_list back to the same file
+        with open(data_file, "w", encoding="utf-8") as f:
+            json.dump(data_list, f, indent=4)
 
     except FileNotFoundError as e:
-        return print(f"File not found: {e}")
+        print(f"File not found: {e}")
 
     except json.JSONDecodeError:
-        return print(f"Error decoding JSON from file: {data_file}")
-
+        print(f"Error decoding JSON from file: {data_file}")
 
 main()
