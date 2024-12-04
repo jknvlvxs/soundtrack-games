416ec565a998be25c90e4b30f09f0c275a13cf36
jknvlvxs <dev.julio.alves@gmail.com>
2024-12-04 14:51:56 -0300

    feat: add metrics for zophar

--- 3. Collecting Youtube Links/zophar/metrics/plots.py
+++ 3. Collecting Youtube Links/zophar/metrics/plots.py
@@ -14,17 +14,16 @@ with open(data_file, "r", encoding="utf-8") as file:
     data = json.load(file)
 
 # Process data
-systems = [game["system"] for game in data]
+systems = [game["console"] for game in data]
 
 years = []
 for game in data:
     try:
-        year = int(
-            game["date"][:4]
-        )  # Get the first 4 characters and convert to integer
-        years.append(year)
+        if(game["release_date"]):
+            year = int(game["release_date"][-4:])
+            years.append(year)
     except ValueError as e:
-        print(f"Error extracting year from date '{game['date']}': {e}")
+        print(f"Error extracting year from date '{game['release_date']}': {e}")
 
 # Count occurrences for graphs
 system_counts = Counter(systems)
@@ -32,27 +31,27 @@ year_counts = Counter(years)
 
 # Create graphs
 # Graph 1: Games per System
-plt.figure(figsize=(10, 5))
+plt.figure(figsize=(16, 8))
 plt.bar(system_counts.keys(), system_counts.values(), color="skyblue")
 plt.title("Number of Games per System")
 plt.xlabel("System")
 plt.ylabel("Number of Games")
-plt.xticks(rotation=45)
+plt.xticks(rotation=45, ha="right")
 plt.tight_layout()
+plt.subplots_adjust(bottom=0.2)
 plt.show()
 
-print(year for year in year_counts.items() if year >= 2025)
-
-filtered_year_counts = {
-    year: count for year, count in year_counts.items() if 1900 <= year <= 2025
-}
+filtered_year_counts = {year: count for year, count in year_counts.items() if 1900 <= year <= 2025}
 
 # Graph 2: Games per Year
-plt.figure(figsize=(10, 5))
+average_games = sum(filtered_year_counts.values()) / len(filtered_year_counts)
+plt.figure(figsize=(16, 8))
 plt.bar(filtered_year_counts.keys(), filtered_year_counts.values(), color="lightgreen")
+plt.axhline(y=average_games, color='red', linestyle=':', label=f'Average: {average_games:.2f}')
 plt.title("Number of Games per Year")
 plt.xlabel("Year")
 plt.ylabel("Number of Games")
 plt.xticks(rotation=45)
 plt.tight_layout()
+plt.legend()
 plt.show()
