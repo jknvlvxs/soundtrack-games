3e45271053c448dd9a5281e2ce2b6b477f97ce90
jknvlvxs <dev.julio.alves@gmail.com>
2025-02-13 09:09:06 -0300

    FIX: file path organization

--- 4. Downloading content/remove_short_mp3.py
+++ 4. Downloading content/remove_short_mp3.py
@@ -7,19 +7,17 @@ def deletar_mp3s(base_dir):
         jogo_path = os.path.join(base_dir, jogo)
         soundtracks_path = os.path.join(jogo_path, "soundtracks")
 
-        if not os.path.isdir(soundtracks_path):
-            continue
-
-        for arquivo in os.listdir(soundtracks_path):
-            if arquivo.startswith("soundtrack_") and arquivo.endswith(".mp3"):
-                file_path = os.path.join(soundtracks_path, arquivo)
-                try:
-                    audio = MP3(file_path)
-                    if audio.info.length < 8:
-                        print(f"{jogo}/soundtracks/{arquivo}")
-                        os.remove(file_path)
-                except Exception as e:
-                    print(f"Erro ao processar {file_path}: {e}")
+        if os.path.isdir(soundtracks_path):
+            for arquivo in os.listdir(soundtracks_path):
+                if arquivo.startswith("soundtrack_") and arquivo.endswith(".mp3"):
+                    file_path = os.path.join(soundtracks_path, arquivo)
+                    try:
+                        audio = MP3(file_path)
+                        if audio.info.length < 8:
+                            print(f"{jogo}/soundtracks/{arquivo}")
+                            os.remove(file_path)
+                    except Exception as e:
+                        print(f"Erro ao processar {file_path}: {e}")
 
 
 if __name__ == "__main__":
