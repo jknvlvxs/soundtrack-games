441908a43dafffaf955c4e07e6627eaa66200227
FelipeMarra <felipeferreiramarra@gmail.com>
2025-03-11 11:21:15 -0300

    task 7 | fix: setting seed and new prompt

--- 7. Video descriptions/test/test_videollamav3.py
+++ 7. Video descriptions/test/test_videollamav3.py
@@ -1,31 +1,40 @@
 import os
 import time
 import shutil
+import random
 import logging
 
+import numpy as np
+
 import torch
+import transformers
 from transformers import AutoModelForCausalLM, AutoProcessor
 
 from params import Params
 
 # Consts
-DEVICE = "cuda:1"
+DEVICE = "cuda:0"
 MODEL_PATH = "DAMO-NLP-SG/VideoLLaMA3-7B"
 ROOT = "/app/dataset/nintendo-snes-spc"
-RES_FOLDER = "./results"
+SEED = 42 # from many seeds tested, this was the best one
+RES_FOLDER = f"./results_seed{SEED}"
 
 # Set of params
 params = [
-    Params(),
-    Params(1, 1),
-    Params(1, 20, "You will receive gameplays and should highlight useful features for creating a song."),
-    Params(5),
-    Params(5, 1),
-    Params(5, 20, "You will receive gameplays and should highlight useful features for creating a song.")
+    #Params(),
+    #Params(1, 1),
+    #Params(1, 20, "You will receive gameplays and should highlight useful features for creating a song."),
+    Params(5, 20),
+    #Params(5, 1),
+    #3Params(5, 20, "You will receive gameplays and should highlight useful features for creating a song.")
 ]
+
+# Set seed
+transformers.set_seed(SEED)
 
 # Videos
 ALADIN = "aladdin/videos/aladdin_00169.mp4"
+ALADIN_CUT = "aladdin/videos/aladdin_00003.mp4"
 AIRCAV = "air-cavalry/videos/air-cavalry_00065.mp4"
 BEETHOVEN = "beethoven-the-ultimate-canine-caper/videos/beethoven-the-ultimate-canine-caper_00061.mp4"
 ALIEN_PREDATOR = "alien-vs-predator/videos/alien-vs-predator_00355.mp4" 
@@ -39,7 +48,8 @@ SUPERR = "super-r-type/videos/super-r-type_00225.mp4"
 SUPERMARIO = "super-mario-all-stars/videos/super-mario-all-stars_00127.mp4"
 MARIOKART= "super-mario-kart/videos/super-mario-kart_00009.mp4"
 
-experiment_videos = [ALADIN, AIRCAV, BEETHOVEN, ALIEN_PREDATOR, ALIEN_PREDATOR_MENU, CAPCOM_SOCCER, CHRONO_DIALOG, DINOCITY, DKC3, FZERO, SUPERR, SUPERMARIO, MARIOKART]
+#experiment_videos = [ALADIN, ALADIN_CUT, AIRCAV, BEETHOVEN, ALIEN_PREDATOR, ALIEN_PREDATOR_MENU, CAPCOM_SOCCER, CHRONO_DIALOG, DINOCITY, DKC3, FZERO, SUPERR, SUPERMARIO, MARIOKART]
+experiment_videos = [ALADIN_CUT]
 
 if not os.path.isdir(RES_FOLDER):
     os.mkdir(RES_FOLDER)
@@ -101,11 +111,19 @@ for exp_vid in experiment_videos:
                 "role": "user",
                 "content": [
                     {"type": "video", "video": {"video_path":cp_video_path, "fps": param.fps}},
-                    {"type": "text", "text": "What are the actions happening in the video?"},
-                    {"type": "text", "text": "How does the game environment look like?"},
-                    {"type": "text", "text": "Describe the game art style."},
-                    {"type": "text", "text": "What is the movement speed in the video?"},
-                    {"type": "text", "text": "What are the game mechanics and its genre?"},
+
+                    # P1
+                    #{"type": "text", "text": "What are the actions happening in the video?"},
+                    #{"type": "text", "text": "How does the game environment look like?"},
+                    #{"type": "text", "text": "Describe the game art style."},
+                    #{"type": "text", "text": "What is the movement speed in the video?"},
+                    #{"type": "text", "text": "What are the game mechanics and its genre?"},
+
+                    # P2
+                    {"type": "text", "text": "What is the type of scene in this gameplay video?"},
+                    {"type": "text", "text": "If it is a menu, a map, or other kind of static scene, describe the possible options, text and background."},
+                    {"type": "text", "text": "If it is a gameplay, describe the actions happening, the environment, the movement speed and the game mechanics."},
+                    {"type": "text", "text": "Describe the game art style and genre."},
                 ]
             }
         ]
--- 7. Video descriptions/videollama3.py
+++ 7. Video descriptions/videollama3.py
@@ -11,10 +11,12 @@ import requests
 from tqdm import tqdm
 
 import torch
+import transformers
 from transformers import AutoModelForCausalLM, AutoProcessor
 
-# Start from cuda:2 and occupy the ones that follow it
-DEVICE_START = 2
+SEED = 42
+# Start from cuda:0 and occupy the ones that follow it
+DEVICE_START = 0
 # One model running takes something like 20GB of VRAM. One A100 can take like 4 in parallel but it is safer to use 3
 PROCESSES_PER_GPU = 3
 # Generate descriptions every STRIDE videos, e.g. if 10 it will take videos 0,10,20,30... which is equivalent to a stride of 9.
@@ -39,6 +41,8 @@ def run_videollama(video_process:tuple[int, str, list[str]]):
         Will create a folder called videos_descriptions in the videos parent dir containing the descricriptions in txt files with the same name
         as the videos files
     """
+    transformers.set_seed(SEED) # Always reset the seed to make every single example more easily reproducible
+
     g_loger = logging.getLogger('global_logger')
 
     pid, gpu, videos_paths = video_process
@@ -76,11 +80,10 @@ def run_videollama(video_process:tuple[int, str, list[str]]):
                     "role": "user",
                     "content": [
                         {"type": "video", "video": {"video_path":video_path, "fps": 5}},
-                        {"type": "text", "text": "What are the actions happening in the video?"},
-                        {"type": "text", "text": "How does the game environment look like?"},
-                        {"type": "text", "text": "Describe the game art style."},
-                        {"type": "text", "text": "What is the movement speed in the video?"},
-                        {"type": "text", "text": "What are the game mechanics and its genre?"},
+                        {"type": "text", "text": "What is the type of scene in this gameplay video?"},
+                        {"type": "text", "text": "If it is a menu, a map, or other kind of static scene, describe the possible options, text and background."},
+                        {"type": "text", "text": "If it is a gameplay, describe the actions happening, the environment, the movement speed and the game mechanics."},
+                        {"type": "text", "text": "Describe the game art style and genre."}
                     ]
                 }
             ]
@@ -153,7 +156,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='videollama3.py')
     # "../5. Database/nintendo-snes-spc/"
     parser.add_argument('--dataset_root', type=str, default="/app/dataset/nintendo-snes-spc", help="path for the dataset games folder")
-    parser.add_argument('--n_processes', type=int, default=15, help="number of processes to run in parallel") 
+    parser.add_argument('--n_processes', type=int, default=9, help="number of processes to run in parallel") 
     args = parser.parse_args()
 
     # Collect videos
