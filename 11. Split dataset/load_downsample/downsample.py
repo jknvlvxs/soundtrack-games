88257946c62619587b3ce6300b74179afc0f14b1
FelipeMarra <felipeferreiramarra@gmail.com>
2025-04-24 17:17:15 -0300

    task 11 | fix: minor changes

--- 11. Split dataset/load_downsample/downsample.py
+++ 11. Split dataset/load_downsample/downsample.py
@@ -26,8 +26,8 @@ def calculate_genres_weights(genre_counts:pd.DataFrame, scaling_factor:int=1.5) 
 
             genre_with_smallest_num_of_segments * scaling_factor
 
-        The scaling factor is a factor that determines how much we tolerate to deviate from the
-        true uniform distribution we could have at the end.
+        The scaling factor determines how much we tolerate to deviate from the true uniform distribution
+        that would otherwise be purely based on the genre with less segments.
 
         Then we divide such `uniform_target` by the amout of segments of each genre to get the weight `w`
 
@@ -72,18 +72,19 @@ def downsample(df:pd.DataFrame):
         # The target number of segments we want to get for this game is the number of
         # segments it has times the weight of its genre
         # In the final sommation it will be as if we took the total number of segments of
-        # the genre and multiplied by the genre weight, achieving our quasi-uniform target distribution
-        n_game_segments = round(n_game_segments * genre_weight)
+        # the genre and multiplied by the genre weight, achieving our uniform_target
+        tgt_game_segments = round(n_game_segments * genre_weight)
 
-        # Now, we need to equally distribute this num_game_segments over the soundtracks of the game
+        # Now, we need to equally distribute this tgt_game_segments over the soundtracks of the game
         soundtracks = game_group["soundtrack"].unique()
         num_soundtracks = len(soundtracks)
-        num_soundtrack_segments = n_game_segments // num_soundtracks
+        tgt_soundtrack_segments = tgt_game_segments // num_soundtracks
+        print(f"Game {game_id}: tgt_soundtrack_segments {tgt_soundtrack_segments} = {tgt_game_segments} // {num_soundtracks}")
 
         # Granting that there is at least one segment per soundtrack
-        num_soundtrack_segments = max(num_soundtrack_segments, 1)
+        tgt_soundtrack_segments = max(tgt_soundtrack_segments, 1)
 
-        # Get num_soundtrack_segments segments for each soundtrack as linearly spaced in time as possible
+        # Get tgt_soundtrack_segments segments for each soundtrack as linearly spaced in time as possible
         # TODO: We cant just utilize linspace because the segments are not linearly spaced
         # We need to use the number of the segment to get where it is in time
         for soundtrack in soundtracks:
@@ -91,14 +92,14 @@ def downsample(df:pd.DataFrame):
 
             # TODO here this soundtrack could have as little as one segment,
             # so this might be one of the reasons why the distribution is alterated
-            if len(soundtrack_segments) <= num_soundtrack_segments:
+            if len(soundtrack_segments) < tgt_soundtrack_segments:
                 sampled = soundtrack_segments
             else:
                 # Sum +2 and then ignore the first and second segments sicne they probably are opening or endin screens
-                indices = np.linspace(0, len(soundtrack_segments), num_soundtrack_segments +2, dtype=int)
+                indices = np.linspace(0, len(soundtrack_segments), tgt_soundtrack_segments +2, dtype=int)
                 indices = indices[1:-1]
 
-                assert num_soundtrack_segments == len(indices), f"{game_id}: num_soundtrack_segments {num_soundtrack_segments} differs from selected indicies in linspace {len(indices)}"
+                assert tgt_soundtrack_segments == len(indices), f"{game_id}: tgt_soundtrack_segments {tgt_soundtrack_segments} differs from selected indicies in linspace {len(indices)}"
 
                 sampled = soundtrack_segments.iloc[indices]
 
